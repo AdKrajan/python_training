96dedb0bcd3c85a3b5a3f5bcbd2e6b9e2332d1e5
AdKrajan <35641554+AdKrajan@users.noreply.github.com>
2018-06-13 12:03:56 +0200

    add HM7 TC delete and modification contacts and modification groups

--- fixture/contact.py
+++ fixture/contact.py
@@ -33,10 +33,37 @@ class ContactHelper:
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
         self.back_to_contacts_list()
 
+
+    def modification_first_contact(self, contact):
+        wd = self.app.wd
+        self.select_first_contact(wd)
+        # find edit button
+        wd.find_element_by_xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(contact.nick)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.home_phone)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.mobile_phone)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        wd.find_element_by_name("update").click()
+        self.back_to_contacts_list()
+
+
     def delete_first_contact(self):
         wd = self.app.wd
         self.select_first_contact(wd)
-        wd.find_element_by_name("selected[]").click()
         # find button delete
         wd.find_element_by_xpath("//div[@id='content']/form[2]/div[2]/input").click()
         wd.switch_to_alert().accept()
