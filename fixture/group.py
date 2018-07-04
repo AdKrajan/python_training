fd988f4d8bdb9645815d166e6eb49fc23be85187
AdKrajan <35641554+AdKrajan@users.noreply.github.com>
2018-07-04 15:55:07 +0200

    test

--- fixture/group.py
+++ fixture/group.py
@@ -12,7 +12,7 @@ class GroupHelper:
         self.open_groups_page()
         # init group creation
         wd.find_element_by_name("new").click()
-        # fill groups form  test
+        # fill groups form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
